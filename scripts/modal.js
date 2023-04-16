841241dc6d1b627e6ece0b8c950b9d023605ccb9
mattbirchler <mattbirchler@users.noreply.github.com>
2023-04-16 14:15:43 -0500

    Click on the text to enter edit mode

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,8 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const openModalBtn = document.getElementById('edit_title');
     const closeModalBtn = document.getElementById('close_modal');
     const modal = document.getElementById('modal');
+    const textArea = document.getElementById('review_text');
 
     openModalBtn.addEventListener('click', () => {
+        var reviewText = localStorage.getItem('media_review');
+        var convertedText = reviewText.split("<br/>").join("\n");
+        modal.style.display = 'block';
+        $('#modal_text_title').val(localStorage.getItem('media_title')).focus();
+        $('#modal_text_meta').val(localStorage.getItem('media_meta'));
+        $('#modal_text_text').val(convertedText);
+    });
+
+    textArea.addEventListener('click', () => {
         var reviewText = localStorage.getItem('media_review');
         var convertedText = reviewText.split("<br/>").join("\n");
         modal.style.display = 'block';
