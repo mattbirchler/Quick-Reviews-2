d74ec8fde0354473013e996800f4c4b8818d9e03
mattbirchler <mattbirchler@users.noreply.github.com>
2024-12-23 14:00:12 -0600

    Reduced animation delay to 30 minutes

--- scripts/should-i-animate.js
+++ scripts/should-i-animate.js
@@ -1,10 +1,11 @@
+const THIRTY_MINUTES = 30 * 60 * 1000; // 30 minutes in milliseconds
+
 function checkLastVisit() {
-    const EIGHT_HOURS = 8 * 60 * 60 * 1000; // 8 hours in milliseconds
     const lastVisit = localStorage.getItem('lastVisitTime');
     const currentTime = new Date().getTime();
     const titleSpans = document.querySelectorAll('#site_title span');
 
-    if (!lastVisit || (currentTime - lastVisit) > EIGHT_HOURS) {
+    if (!lastVisit || (currentTime - lastVisit) > THIRTY_MINUTES) {
         titleSpans.forEach(span => {
             span.classList.add('animate__jackInTheBox');
             span.style.opacity = '1.0';
